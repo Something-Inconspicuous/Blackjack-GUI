2c8a345f0fc4c2b78573341b3755aef67a6ec072
Something-Inconspicuous <webhead220@gmail.com>
2023-01-05 23:54:39 -0500

    Added Card and Deck
    Added my own code for card and deck.

---
+++ Card.java
@@ -0,0 +1,88 @@
+/**
+ * The {@code Card} class represents a single card per instance.
+ * <p>
+ * Each card stores its own suit and face as integers
+ * 
+ * @see #getFace()
+ * @see #getSuit()
+ */
+public class Card {
+    private int suit = -1;
+    private int face = -1;
+  
+    /**
+     * Creates a card with a suit s and face value f
+     * 
+     * @param s -- the suit of the card. 1 is Clubs, 2 is Diamonds, 3 is Hearts, and 4 is Spades
+     * @param f -- the face value of the card
+     */
+    public Card(int s, int f){
+      suit = s;
+      face = f;
+    }
+  
+    /**
+     * Gives a string in the format of "Face of Suit"
+     *
+     * @return {@code String} -- the card as a String 
+     */
+    @Override
+    public String toString(){
+      String suitStr = "Error";
+      //if something goes wrong and the card strings are messed up, it will say error
+      
+      switch(suit){
+        case 4:
+          suitStr = "Spades";
+          break;
+  
+        case 3:
+          suitStr = "Hearts";
+          break;
+  
+        case 2:
+          suitStr = "Diamonds";
+          break;
+  
+        case 1:
+          suitStr = "Clubs";
+      }
+  
+      if(face < 11 && face != 1 && face != 14){
+        return face + " of " + suitStr;
+      } else if(face == 11){
+        return "Jack of " + suitStr;
+      } else if(face == 12){
+        return "Queen of " + suitStr;
+      } else if(face == 13){
+        return "King of " + suitStr;
+      }
+      return "Ace of " + suitStr;
+    }
+  
+    /**
+     * Returns the face value of the card
+     * 
+     * @return {@code int} -- the face value of the card
+     */
+    public int getFace(){
+      return face;
+    }
+  
+    /**
+     * Returns the suit of the card
+     * <p>
+     * <pre>
+     * The suit is stored as in integer where 
+     * 1: Clubs
+     * 2: Diamonds
+     * 3: Hearts
+     * 4: Spades
+     * </pre>
+     * 
+     * @return {@code int} -- the suit of the card as an int
+     */
+    public int getSuit(){
+      return suit;
+    }
+  }
